f19ede64689dcb758cb70811864656d89ffa3d6e
Johannes Mesner <johannes.mesner@tum.de>
2023-01-03 06:07:46 +0100

    AI funktioniert, small bug fixxes aswell

--- src/app/arschloch/arschloch.component.ts
+++ src/app/arschloch/arschloch.component.ts
@@ -1,37 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import {animate, keyframes, style, transition, trigger} from "@angular/animations";
-import {CarddeckService} from "../carddeck.service";
+
 
 @Component({
   selector: 'app-arschloch',
   templateUrl: './arschloch.component.html',
   styleUrls: ['./arschloch.component.css'],
-  animations: [
-    trigger('cardIn', [
-    transition(':enter', [
-      animate('400ms', keyframes([
-        style({transform: 'translateY(1000px)', offset: 0}),
-        style({transform: 'translateY(0) translateX(0)', offset: 1}),
-      ]))])])]
+  animations: []
 })
 
 export class ArschlochComponent implements OnInit {
-  cardsPlayer: any[] = [];
 
 
-  constructor(private cardDeck: CarddeckService) {
+  constructor() {
   }
   ngOnInit(): void {
-    this.dealCards();
   }
 
-  private dealCards() {
-    let deck = this.cardDeck.getShuffledDeck();
-    for (let i = 0; i < 8; i++) {
-      setTimeout(() => {
-        this.cardsPlayer.push(deck.pop());
-      }, i * 500);
-    }
-  }
+
 
 }
